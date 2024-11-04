14ca8ea2b068ceba695625cef74516be362fa0cb
sreenu926 <89065670+sreenu926@users.noreply.github.com>
2024-11-04 12:38:00 +0530

    Update script.js
    // 3. Helper Component Definitions: Three stateless functional components are defined for reusability:
                // a: Renders a toolbar button with text, icon, and click handler.
                // n: Renders a textarea element for the editor.
                // i: Renders a div element displaying the parsed HTML content from the Markdown using marked and the custom renderer.

--- script.js
+++ script.js
@@ -110,6 +110,11 @@
     }
   }
 
+        // 3. Helper Component Definitions: Three stateless functional components are defined for reusability:
+            // a: Renders a toolbar button with text, icon, and click handler.
+            // n: Renders a textarea element for the editor.
+            // i: Renders a div element displaying the parsed HTML content from the Markdown using marked and the custom renderer.
+    
   const a = (e) =>
       React.createElement(
         "div",
@@ -123,6 +128,7 @@
 
         React.createElement("i", { className: e.icon, onClick: e.onClick })
       ),
+      
     n = (e) =>
       React.createElement("textarea", {
         id: "editor",
@@ -130,6 +136,7 @@
         type: "text",
         value: e.markdown,
       }),
+      
     i = (t) =>
       React.createElement("div", {
         dangerouslySetInnerHTML: {
@@ -137,6 +144,7 @@
         },
         id: "preview",
       }),
+      
     r =
       "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n";
 
